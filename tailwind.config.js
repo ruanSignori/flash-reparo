aba6bdce8e0d2561f5a4f882ba402779f05147c5
ruan <cleosignori@gmail.com>
2025-01-19 02:16:10 -0300

    adicionado header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ export default {
       fontFamily: {
         sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: 'var(--color-primary)',
+        bg: {
+          DEFAULT: 'var(--color-bg)',
+          light: 'var(--color-bg-light)',
+          gray: {
+            DEFAULT: 'var(--color-bg-gray)',
+            light: 'var(--color-bg-gray-light)',
+          },
+        },
+        black: 'var(--color-black)',
+        white: 'var(--color-white)',
+        green: 'var(--color-green)',
+        red: 'var(--color-red)',
+        gray: 'var(--color-gray)'
+      },
     },
   },
   plugins: [],
