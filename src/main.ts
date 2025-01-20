567439003e52cdc07cbc1d8caee127d4d3fab20d
Ruan <ruan@solis.com.br>
2025-01-20 13:18:46 -0300

    ajustado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './styles/globals.css';
 
 import '@fontsource/montserrat/800.css'; 
+import '@fontsource/montserrat/600.css'; 
 import '@fontsource/montserrat/500.css'; 
 import '@fontsource/montserrat';
 
