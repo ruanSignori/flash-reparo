2981ae05df8503666f97720916fb5dfc4c515f4e
ruan <ruan.preto@universo.univates.br>
2025-01-26 15:22:16 -0300

    ajustado renderização do obrigado.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  content: ['./index.html', 'obrigado.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
