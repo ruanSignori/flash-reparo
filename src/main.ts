d16530cd708ad0a297c2dd39ca9cd52963076da7
ruan <ruan.preto@universo.univates.br>
2025-01-28 00:17:29 -0300

    adicionado animações

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,16 @@ import '@fontsource/montserrat';
 
 import "@phosphor-icons/web/bold";
 import "@phosphor-icons/web/fill";
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.isIntersecting ? 
+    entry.target.classList.add('animate-visible'):
+    entry.target.classList.remove('animate-visible')
+  });
+  
+},  { threshold: 0.1 });
+
+const animatedElements = document.querySelectorAll('.animate-hidden');
+animatedElements.forEach((e) => observer.observe(e));
