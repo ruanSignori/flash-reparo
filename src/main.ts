143c4eda811e7e2f20ff936864569642f1ca8f03
Ruan <ruan@solis.com.br>
2025-02-04 12:52:28 -0300

    deprecated: removido importação de fonte depreciada

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import './styles/globals.css';
 import 'lite-youtube-embed/src/lite-yt-embed.css';
 import 'lite-youtube-embed/src/lite-yt-embed.js';
 
-
-import '@fontsource/montserrat/800.css'; 
-import '@fontsource/montserrat/600.css'; 
-import '@fontsource/montserrat/500.css'; 
-import '@fontsource/montserrat';
-
 import "@phosphor-icons/web/bold";
 import "@phosphor-icons/web/fill";
 
