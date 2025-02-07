0e6356c749cadb8ff9c3f323a7e12afc3fcd7f39
Ruan <ruan@solis.com.br>
2025-02-07 10:51:49 -0300

    ajustado

--- src/main.ts
+++ src/main.ts
@@ -20,33 +20,3 @@ animatedElements.forEach((e) => observer.observe(e));
 
 // Afiliados
 // Comentado pq da erro no deploy
-// function getAffiliateParams() {
-
-//   console.log('Xgrow Affiliate Params Loaded');
-
-//   const affiliateId = new URLSearchParams(window.location.search).get('xa');
-
-//   return { platformId: 'b2198636-5591-4ef9-9942-7c40df45f1a9', planId: '75920', affiliateId }
-
-// }
-
-const affiliateId = new URLSearchParams(window.location.search).get("xa");
-
-if (affiliateId) {
-
-  localStorage.setItem('affiliateId', affiliateId)
-
-}
-
-const elementosHref = document.querySelectorAll('[href]')
-
-
-
-elementosHref.forEach(elemento => {
-
-  const hrefAtual = elemento.getAttribute('href')
-  console.log(hrefAtual)
-  if (!hrefAtual) return;
-  elemento.setAttribute('href', hrefAtual + '?xa=' + localStorage.getItem('affiliateId'))
-
-})
