fc7cb8349ff0d514693fce3e846e21cc9f650d2c
ruan <ruan.preto@universo.univates.br>
2025-02-06 22:28:00 -0300

    ajustado

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,36 @@ const observer = new IntersectionObserver((entries) => {
 
 const animatedElements = document.querySelectorAll('.animate-hidden');
 animatedElements.forEach((e) => observer.observe(e));
+
+// Afiliados
+
+function getAffiliateParams() {
+
+  console.log('Xgrow Affiliate Params Loaded');
+
+  const affiliateId = new URLSearchParams(window.location.search).get('xa');
+
+  return { platformId: 'b2198636-5591-4ef9-9942-7c40df45f1a9', planId: '75920', affiliateId }
+
+}
+
+const affiliateId = new URLSearchParams(window.location.search).get("xa");
+
+if (affiliateId) {
+
+  localStorage.setItem('affiliateId', affiliateId)
+
+}
+
+const elementosHref = document.querySelectorAll('[href]')
+
+
+
+elementosHref.forEach(elemento => {
+
+  const hrefAtual = elemento.getAttribute('href')
+  console.log(hrefAtual)
+  if (!hrefAtual) return;
+  elemento.setAttribute('href', hrefAtual + '?xa=' + localStorage.getItem('affiliateId'))
+
+})
